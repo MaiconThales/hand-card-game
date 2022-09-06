2413f89458658a7cbac64d85e8745854aa2c3628
Maicon Thales <maiconthales34@hotmail.com>
2022-09-06 16:34:04 -0300

    new Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,13 @@ export const environment = {
   production: true,
 
   firebaseConfig: {
-    apiKey: "AIzaSyDnsNI7bchSWrOxCNITw_Yz7buWDdOpvQw",
-    authDomain: "hand-card-de3fe.firebaseapp.com",
-    projectId: "hand-card-de3fe",
-    storageBucket: "hand-card-de3fe.appspot.com",
-    messagingSenderId: "664938964814",
-    appId: "1:664938964814:web:cda86cdee526d8273b201f"
+    apiKey: "AIzaSyADSBG1-lt0Zy7VB04HttODR6C2GjjeVwE",
+    authDomain: "hand-card-game.firebaseapp.com",
+    databaseURL: "https://hand-card-game-default-rtdb.firebaseio.com",
+    projectId: "hand-card-game",
+    storageBucket: "hand-card-game.appspot.com",
+    messagingSenderId: "405608957685",
+    appId: "1:405608957685:web:98b837270b075ee68667bc"
   },
 
   REDIRECT_BOARD: '/board',
